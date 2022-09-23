45cc8b1ea3152015d77efe2a93fa2c24a6f4659d
geunsoo <geunsoo1106@gmail.com>
2022-09-23 11:34:59 +0900

    test

---
+++ src/App/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+
+function App() {
+  const name = 'tom';
+  const naver = {
+    name: '네이버',
+    url: 'http://naver.com',
+  };
+  return (
+    <div className='App'>
+      <h1
+        style={{
+          color: 'red',
+          backgroundColor: 'blue',
+        }}
+      >
+        Hello, {name}
+      </h1>
+      <a href={naver.url}>{naver.name}</a>
+    </div>
+  );
+}
+
+export default App;
