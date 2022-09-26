52cbfe2adaacf5f1f97ff4c3c69f4b0f984f9203
geunsoo <geunsoo1106@gmail.com>
2022-09-26 11:45:13 +0900

    리액트 설치 순서

---
+++ src/App/reactStart.js
@@ -0,0 +1,10 @@
+// 리액트 설치 순서
+// npm install -g create-react-app
+// --> 에러발생시
+// sudo npm install create-react-app
+// 설치확인--> create-react-app -V
+
+// 폴더에 개발환경 만들기
+// cd 파일경로 --> create-react-app
+// 실행
+// npm run start
